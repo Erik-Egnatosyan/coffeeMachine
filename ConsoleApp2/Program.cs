9491e9dce18ac4eb4fa184a6ab22ce41813c8deb
Sansacia <egnatosyan.sans@mail.ru>
2022-06-17 21:40:54 +0400

    Normal text edition

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -12,17 +12,17 @@ namespace ConsoleApp2
         {
             byte maccoffee = 100;
             byte cappuccino = 150;
-            byte expresso = 200;
+            byte espresso = 200;
             byte darkCoffee = 250;
 
-            Console.WriteLine("Pls insert the coin. Mac:100, Capp:150, Expr:200, Dark: 250;");
+            Console.WriteLine("Pls insert the coin and choise the coffee: ");
+            Console.WriteLine($"Maccoffee costs {maccoffee}\nCappuccino costs {cappuccino}\nEspresso costs {espresso}\nDarkcoffee costs {darkCoffee}");
             byte money = byte.Parse(Console.ReadLine());
-
-            Console.WriteLine("Pls choose the drink");
-            Console.WriteLine($"Write 1 if you want maccoffee. Price - {maccoffee}");
-            Console.WriteLine($"Write 2 if you want cappuccino. Price - {cappuccino}");
-            Console.WriteLine($"Write 3 if you want expresso. Price - {expresso}");
-            Console.WriteLine($"Write 4 if you want darkCoffee. Price - {darkCoffee}");
+            Console.WriteLine("Pls choise the coffee: ");
+            Console.WriteLine($"Write 1 if you want maccoffee.");
+            Console.WriteLine($"Write 2 if you want cappuccino.");
+            Console.WriteLine($"Write 3 if you want expresso.");
+            Console.WriteLine($"Write 4 if you want darkcoffee.");
             byte number = byte.Parse(Console.ReadLine());
 
             if (number == 1)
@@ -57,15 +57,15 @@ namespace ConsoleApp2
             }
             else if (number == 3)
             {
-                while (money < expresso)
+                while (money < espresso)
                 {
-                    Console.WriteLine($"Pls add more money {expresso - money}");
+                    Console.WriteLine($"Pls add more money {espresso - money}");
                     byte moreMoney = byte.Parse(Console.ReadLine());
                     money += moreMoney;
                 }
-                while (money > expresso)
+                while (money > espresso)
                 {
-                    Console.WriteLine($"You entered too much money, take your change {money - expresso}");
+                    Console.WriteLine($"You entered too much money, take your change {money - espresso}");
                     break;
                 }
                 Console.WriteLine("Take your coffee");
